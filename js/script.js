428c3729e626b86dd3cd1ca1bc1429df422c268a
jacopo35 <jaczor00@gmail.com>
2021-11-23 15:06:04 +0100

    add-js-basic-instructions

--- js/script.js
+++ js/script.js
@@ -9,3 +9,20 @@ MILESTONE 3
 Applica uno stile differente a seconda del valore dell’indice per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5.
  */
 
+// Scrivere un programma che stampi in console i numeri da 1 a 100
+let i;
+
+//creare un ciclo for da 1 a 100
+for (i = 1; i <= 100; i++) {
+
+//creare un if per le condizioni dei multipli
+if (i % 3 == 0 && i % 5 !== 0) { //resto == 0 allora multiplo di 3
+        console.log('Frizz');
+ } else if (i % 3 !== 0 && i % 5 == 0) { //resto == 0 allora multiplo di 5
+        console.log('Buzz');
+} else if (i % 3 == 0 && i % 5 == 0) { //multipli di 15
+        console.log('FrizzBuzz');
+} else {
+        console.log(i);
+ }
+}
